3628d607ff7fc070e5ab954125554de7694d5cb4
Sujith <somraajsujith@gmail.com>
2020-04-28 16:21:47 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      name : ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({[e.target.name]:e.target.value});
+  }
+
+  handleSubmit(){
+    if(this.state.name !== ''){
+      alert('Submitted');
+    }
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{this.state.value}</p>
+      <input name="name" value={this.state.value} onChange={this.handleChange} />
+      <button onClick={this.handleSubmit}>Check</button>
     </div>
   );
+  }
 }
 
-export default App;
